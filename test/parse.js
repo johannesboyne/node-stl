32a03bd8426cca0407c6bd15690dbc62ff8de8d4
redwildfire13 <rose.joseph42@gmail.com>
2016-09-05 11:39:01 +0800

    Added surface area test
    Tests the box for correct values. Using box because these values can be easily calculated by hand.

--- test/parse.js
+++ test/parse.js
@@ -2,7 +2,7 @@ var
 assert								= require('assert'),
 NodeStl								= require('../');
 
-describe('should load an STL and measure volume and weight', function () {
+describe('should load an STL and measure volume, weight, and area', function () {
 	it('load an ascii file', function() {
 		// source for this stl: http://www.thingiverse.com/thing:47956
 		var a = new NodeStl(__dirname + '/test_data/WALLY_1plate.stl');
@@ -18,5 +18,11 @@ describe('should load an STL and measure volume and weight', function () {
 		var c = new NodeStl(__dirname + '/test_data/Pterosaur_FDM.stl');
     assert.deepEqual(c.boundingBox.map(function (bbe) { return Math.round(bbe) }), 
       [60,45,50])
+	});
+	it('loads the box_2x3x4 with area of 52 and volume of 24', function() {
+		// source for this stl: http://www.thingiverse.com/thing:1607628
+		var d = new NodeStl(__dirname + '/test_data/box_2x3x4.stl');
+    		assert.equal(d.area, 52);
+    		assert.equal(d.volume, 24);
 	});
 });
