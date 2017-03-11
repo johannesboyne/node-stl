df2ba700b50af310d3db3b6c184d5ce3be3973e2
Alex J Vazhatharayil <alexjv89@gmail.com>
2017-03-11 17:19:04 +0530

    ability to work with files from remote url

--- README.md
+++ README.md
@@ -13,6 +13,24 @@ console.log(stl.boundingBox,'(mm)');  // [60,45,50] (mm)
 ```
 node-stl recognizes by itself whether it is dealing with an ASCII STL or a binary STL file
 
+## load file from url
+
+Use `request` to load a file from url
+
+```javascript
+var request=require('request');
+var requestSettings = {
+   method: 'GET',
+   url: 'https://s3.amazonaws.com/minifactory-stl/WALLY_1plate.stl',
+   encoding: null,
+};
+request(requestSettings, function(error, response, body) {
+    var stl = new NodeStl(body);
+    assert.equal(stl.volume, 21.87511539650792);
+    done(null);
+});
+```
+
 ## install
 
 use [npm](https://npmjs.org):
--- test/parse.js
+++ test/parse.js
@@ -1,6 +1,7 @@
 var 
 assert								= require('assert'),
 NodeStl								= require('../');
+request								= require('request');
 
 describe('should load an STL and measure volume and weight', function () {
 	it('load an ascii file', function() {
@@ -19,4 +20,22 @@ describe('should load an STL and measure volume and weight', function () {
     assert.deepEqual(c.boundingBox.map(function (bbe) { return Math.round(bbe) }), 
       [60,45,50])
 	});
+	it('loads a file buffer', function() {
+		var fs = require('fs');
+		var file_buf = fs.readFileSync(__dirname + '/test_data/WALLY_1plate.stl');
+		var a = new NodeStl(file_buf);
+		assert.equal(a.volume, 21.87511539650792);
+	});
+	it('loads a file from url',function(done){
+		var requestSettings = {
+		   method: 'GET',
+		   url: 'https://s3.amazonaws.com/minifactory-stl/WALLY_1plate.stl',
+		   encoding: null,
+		};
+		request(requestSettings, function(error, response, file) {
+			var a = new NodeStl(file);
+			assert.equal(a.volume, 21.87511539650792);
+			done(null);
+		});
+	}).timeout('5000');
 });
